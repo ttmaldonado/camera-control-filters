41bbb69c19084513495320bd09e465480bb9b064
Thiago Maldonado <cin_tmaldonado@uolinc.com>
2017-11-23 18:51:07 -0200

    Criando estrutura do projeto

---
+++ app/src/main/java/com/example/maldonado/cvtest1/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.maldonado.cvtest1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (!OpenCVLoader.initDebug()) {
+            Log.e(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), not working.");
+        } else {
+            Log.d(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), working.");
+        }
+
+
+    }
+}
